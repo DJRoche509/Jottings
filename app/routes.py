f090dfa330cf1ff5caf2c46426cbc69f6e77f4fe
DJRoche509 <davjroche@gmail.com>
2023-10-27 07:24:39 -0400

    Add home route definition

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,10 @@
-from run import app
+from flask import render_template
+
+# Import the Flask app instance from app package
+from app import app  
+
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('index.html')
