fd66b3049b91fb79161617ea018bd186a6cd9899
DJRoche509 <davjroche@gmail.com>
2023-10-27 07:25:00 -0400

    Refactor a bit

--- run.py
+++ run.py
@@ -1,10 +1,17 @@
-from flask import Flask
+# Import the Flask app instance from app package
+from app import app     
+
 import config
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = config.SECRET_KEY
-
-from routes import *
 
 if __name__ == '__main__' :
-    app.run(debug = True)
+    print(f'Jottings App is Jamming on port: {config.PORT}')
+    app.run(debug = True, port = config.PORT)
+
+
+
+
+''''
+In `run.py`, we're importing the `app` object from the `app` package and running 
+the Flask development server on the specified port from the `config.py` file
+'''
