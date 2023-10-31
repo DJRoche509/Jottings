a81866be8037f89c6594c775807a962a9a0e2ab7
DJRoche509 <davjroche@gmail.com>
2023-10-31 03:03:41 -0400

    Add DeleteNoteForm class

--- app/forms.py
+++ app/forms.py
@@ -5,3 +5,7 @@ from wtforms.validators import DataRequired
 class AddNoteForm( FlaskForm ):
     title = StringField( "Title", validators=[ DataRequired() ] )
     submit = SubmitField( 'Submit' )
+
+
+class DeleteNoteForm(FlaskForm):
+    submit = SubmitField('Delete')
